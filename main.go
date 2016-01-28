1083be8f42713b35602fa2fc6646fb6aff215554
James Moore <james@knowthen.com>
2016-01-28 15:34:28 -0600

    Event Router

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
+
 	"github.com/mitchellh/mapstructure"
 	"net/http"
 	"time"
@@ -18,66 +18,11 @@ type Channel struct {
 	Name string `json:"name"`
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	router := NewRouter()
+
+	router.Handle("channel add", addChannel)
+
+	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Hello from go")
-	socket, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		var inMessage Message
-		var outMessage Message
-		if err := socket.ReadJSON(&inMessage); err != nil {
-			fmt.Println(err)
-			break
-		}
-		fmt.Printf("%#v\n", inMessage)
-		switch inMessage.Name {
-		case "channel add":
-			err := addChannel(inMessage.Data)
-			if err != nil {
-				outMessage = Message{"error", err}
-				if err := socket.WriteJSON(outMessage); err != nil {
-					fmt.Println(err)
-					break
-				}
-			}
-		case "channel subscribe":
-			go subscribeChannel(socket)
-		}
-	}
-}
-func addChannel(data interface{}) error {
-	var channel Channel
-
-	err := mapstructure.Decode(data, &channel)
-	if err != nil {
-		return err
-	}
-	channel.Id = "1"
-	fmt.Println("added channel")
-	return nil
-}
-
-func subscribeChannel(socket *websocket.Conn) {
-	// TODO: rethinkDB Query / changefeed
-	for {
-		time.Sleep(time.Second * 1)
-		message := Message{"channel add",
-			Channel{"1", "Software Support"}}
-		socket.WriteJSON(message)
-		fmt.Println("sent new channel")
-	}
 }
---
+++ router.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+)
+
+type Handler func(*Client, interface{})
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+type Router struct {
+	rules map[string]Handler
+}
+
+func NewRouter() *Router {
+	return &Router{
+		rules: make(map[string]Handler),
+	}
+}
+
+func (r *Router) Handle(msgName string, handler Handler) {
+	r.rules[msgName] = handler
+}
+
+func (e *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	client := NewClient(socket)
+	go client.Write()
+	client.Read()
+
+}
