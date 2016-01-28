27f0c5ce8fef3ee3a61d9927051578fa1c71513a
James Moore <james@knowthen.com>
2016-01-28 15:55:22 -0600

    Channel Subscription Event Handler

--- handlers.go
+++ handlers.go
@@ -21,3 +21,23 @@ func addChannel(client *Client, data interface{}) {
 		}
 	}()
 }
+
+func subscribeChannel(client *Client, data interface{}) {
+	go func() {
+		cursor, err := r.Table("channel").
+			Changes(r.ChangesOpts{IncludeInitial: true}).
+			Run(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+			return
+		}
+		var change r.ChangeResponse
+		for cursor.Next(&change) {
+			if change.NewValue != nil && change.OldValue == nil {
+				client.send <- Message{"channel add", change.NewValue}
+				fmt.Println("sent channel add msg")
+			}
+		}
+	}()
+
+}
--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	router := NewRouter(session)
 
 	router.Handle("channel add", addChannel)
-
+	router.Handle("channel subscribe", subscribeChannel)
 	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
 }
