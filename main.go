d5fb9382ba1ede586b8c06b73b073cc50cee3e8f
James Moore <james@knowthen.com>
2016-01-28 14:47:53 -0600

    Go Web Server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello from go")
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(":4000", nil)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from go")
 }
